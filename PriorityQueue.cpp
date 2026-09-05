b9e53ac455a1261d3e9f30394c314a3ca6544b38
kratik <kratik437@gmail.com>
2026-09-06 00:34:32 +0530

    PriorityQueue

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -11,10 +11,28 @@ int main()
 
     //Min Heap 
     priority_queue<int, vector<int>, greater<int> > mini;
-    maxi.push(0);
     maxi.push(1);
-    maxi.push(2);
     maxi.push(3);
+    maxi.push(2);
+    maxi.push(0);
+    int n = maxi.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << maxi.top() << endl; //front element
+        maxi.pop();
+    }
+    mini.push(5);
+    mini.push(1);
+    mini.push(0);
+    mini.push(3);
+    int n = maxi.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << mini.top() << endl;
+        mini.pop();
+    }
+    
+    
     
 
 
