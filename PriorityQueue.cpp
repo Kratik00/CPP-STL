ef7c5b936c220fde946e8c72fa62b57f86edeaca
kratik <kratik437@gmail.com>
2026-09-05 23:13:56 +0530

    PriorityQueue

---
+++ PriorityQueue.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+
+using namespace std;
+int main()
+{
+    //MAx Heap
+    priority_queue<int> maxi;
+
+    //Min Heap 
+    priority_queue<int, vector<int>, greater<int> > mini;
+    maxi.push(0);
+    maxi.push(1);
+    maxi.push(2);
+    maxi.push(3);
+    
+
+
+}
